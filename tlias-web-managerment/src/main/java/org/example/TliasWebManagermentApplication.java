379c3450b8b23e08c7f1ff6b78903638c601bf82
SZJ-star <2686236895@qq.com>
2026-09-10 20:01:48 +0800

    1

---
+++ tlias-web-managerment/src/main/java/org/example/TliasWebManagermentApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasWebManagermentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasWebManagermentApplication.class, args);
+    }
+
+}
